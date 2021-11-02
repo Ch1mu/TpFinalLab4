d1ebdcddaad462708eca9cefbc6ba573b3f96951
Ch1mu <frannluza@gmail.com>
2021-11-02 18:25:59 -0300

    Revert "Merge branch 'master' of https://github.com/Ch1mu/TpFinalLab4"
    This reverts commit 63656644eb96a666419923b88149682ae6b63dce, reversing
    changes made to 2ba0a4d065fc4a0a77096ed2ffbbcd7bff87db0e.

--- TPFINALFINAL/jobApply.php
+++ TPFINALFINAL/jobApply.php
@@ -22,6 +22,7 @@ unlink("Data/accounts.json");
 
 foreach($companyList as $company)
 {
+<<<<<<< HEAD
     if($company->getEmail() <> $_SESSION["email"])
     {
         $companyDAO->add($company);
@@ -33,6 +34,16 @@ foreach($companyList as $company)
             echo '<script language="javascript">alert("te has postulado Exitosamente");';
             echo "window.location = '../mainUser.php'; </script>";
     }
+=======
+if($_SESSION["job"] == 0){
+    $_SESSION["job"] = $jobID;
+}
+else{
+    echo '<script language="javascript">alert("Ya te has postulado a un trabajo anteriormente");';
+    echo "window.location = 'Views/Jobs.php'; </script>";
+    asjdgkjasgfasd
+}
+>>>>>>> parent of 6365664 (Merge branch 'master' of https://github.com/Ch1mu/TpFinalLab4)
 }
     
 
