8582bedad629a6cc067e3a95ab167531c45c7e3b
LucianoSoto <lusoto456@gmail.com>
2021-11-02 18:07:29 -0300

    A ver

--- TPFINALFINAL/Views/filterCompany.php
+++ TPFINALFINAL/Views/filterCompany.php
@@ -15,7 +15,7 @@ if($_SESSION["email"] == "admin@utn.com")
                          <?php
                               foreach($studentList as $company)
                               {
-                                  if($company->getNombre() == $nombre){
+                                  if(str_contains($nombre, $company->getNombre())){
                                    ?>
                                         <details class ="btn btn-primary ml-auto d-block">
                                                 <summary>
