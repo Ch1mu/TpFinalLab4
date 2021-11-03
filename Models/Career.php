bb67049d4413440a3df29f930ff414ae14b93de7
LucianoSoto <lusoto456@gmail.com>
2021-11-03 16:30:29 -0300

    Career Model

---
+++ Models/Career.php
@@ -0,0 +1,32 @@
+<?php
+    namespace Models;
+
+    class Career{
+        private $careerId;
+        private $description;
+        private $active;
+
+	function getActive() { 
+ 		return $this->active; 
+	} 
+
+	function setActive($active) {  
+		$this->active = $active; 
+	} 
+
+	function getDescription() { 
+ 		return $this->description; 
+	} 
+
+	function setDescription($description) {  
+		$this->description = $description; 
+	} 
+
+	function getCareerId() { 
+ 		return $this->careerId; 
+	} 
+
+	function setCareerId($careerId) {  
+		$this->careerId = $careerId; 
+	} 
+}
