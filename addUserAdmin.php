d9e57e64fb96e5a9cf0e54266fb081f4b726c1c1
LucianoSoto <lusoto456@gmail.com>
2021-11-11 18:42:55 -0300

    add user para admin

---
+++ addUserAdmin.php
@@ -0,0 +1,38 @@
+<?php
+    require_once('nav.php');
+    require_once "logged.php";
+?>
+<main class="py-5">
+     <section id="listado" class="mb-5">
+          <div class="container">
+          
+               
+               <h2 class="mb-4">Agregar Usuario</h2>
+               <form action="<?php echo FRONT_ROOT ?>adduser.php" method="post" class="bg-light-alpha p-5">
+                    <div class="row">                         
+                         
+                         <div class="col-lg-4">
+                              <div class="form-group">
+                                   <label for="">Email</label>
+                                   <input type="text" name="email" value="" class="form-control">
+                              </div>
+                         </div>
+                         <div class="col-lg-4">
+                              <div class="form-group">
+                                   <label for="">Contraseña</label>
+                                   <input type="text" name="password" value="" class="form-control">
+                              </div>
+                         </div>
+                         <div class="col-lg-4">
+                              <div class="form-group">
+                                   <label for="">Rol</label>
+                                   <input type="text" name="role" value="" class="form-control">
+                              </div>
+                         </div>
+                    </div>
+                    <button type="submit" class="btn btn-dark ml-auto d-block">Agregar</button>
+               </form>
+          </div>
+          
+     </section>
+</main>
